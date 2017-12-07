8865df5666b57229cba95f8eb6c8d6e1add46862
Marcia da Silva <marcia.dasilva@unijui.edu.br>
2017-12-07 16:57:40 -0200

    layout separado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,52 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="{{ asset('css/app.css')}}" type="text/css">
-  <title>5ª Essência</title>
-</head>
-<body>
+@extends('layouts.master')
+@section('title', '5ª Essência')
 
-  <div class="modal simpleModal">
-    <div class="modal-content">
-      <div class="modal-header">
-        <span class="closeBtn">&times;</span>
-        <h2>5ª Essência Gastronomia e Eventos</h2>
-      </div>
-      <div class="modal-body">
-        <p>Entre em contato conosco</p>
-        <p>Empresa especializada em eventos, festas, corporativos, serviços de alimentação e elaboração de menus especiais, treinamentos e consultorias sob a coordenação do Chef Internacional André Pinheiro Machado. Experiência em Cozinha do Mundo, Alta Gastronomia e Excelência no atendimento das áreas gastronômicas.</p>
-      </div>
-      <div class="modal-footer">
-        <h3>5ª Essência Gastronomia e Consultoria</h3>
-      </div>
-    </div>
-  </div>
-
-  <div class="modal simpleModal">
-    <div class="modal-content">
-      <div class="modal-header">
-        <span class="closeBtn">&times;</span>
-        <h2>5ª Essência Gastronomia e Eventos</h2>
-      </div>
-      <div class="modal-body">
-
-      </div>
-      <div class="modal-footer">
-        <h3>5ª Essência Gastronomia e Consultoria</h3>
-      </div>
-    </div>
-  </div>
-
-  <!-- Header Showcase -->
-  <header id="showcase" class="grid">
-    <div class="bg-image"></div>
-    <div class="content-wrap">
-      <a href="#section-b" class="button">Conheça nosso cardápio</a>
-    </div>
-  </header>
+@section('content')
 
   <!-- Main Area -->
   <main id="main">
@@ -123,7 +78,7 @@
     <!-- Section D -->
     <section id="section-d" class="grid">
       <div class="content-wrap button-position">
-        <a href="#" class="button-ristretto">Conheça nosso bar café Ristretto</a>
+        <a href="#" class="button-ristretto">Conheça café Ristretto</a>
         <img src="img/ristretto.jpg" alt="ristretto">
       </div>
     </section>
@@ -142,14 +97,4 @@
       </div>
     </section>
   </main>
-
-  <!-- Footer -->
-  <footer id="main-footer" class="grid">
-    <div>5ª Essência Gastronomia e Eventos</div>
-    <div>Project By <a href="#" target="_blank">Marcia da Silva</a></div>
-  </footer>
-
-  <script src="main.js"></script>
-
-</body>
-</html>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+
+
+<!-- Footer -->
+<footer id="main-footer" class="grid">
+  <div>5ª Essência Gastronomia e Eventos</div>
+  <div>Project By <a href="#" target="_blank">Marcia da Silva</a></div>
+</footer>
