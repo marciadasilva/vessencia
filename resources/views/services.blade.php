3b5e3ee7d98f0ce95773af7ebe1ebfb95dd8f202
marcia <marcia.dasilva1410@gmail.com>
2017-12-17 13:58:36 -0200

    atualizacao

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -7,13 +7,13 @@
 <section id="services">
     <header> Teste </header>
     <div>
-        <a href=""><div><img src="{{asset('img/restaurante.jpg')}}" alt=""><span>Serviço 1</span></img></div></a>
+        <a href=""><div><img src="{{asset('img/restaurante.jpg')}}" alt=""><span>Serviço 1</span></div></a>
     </div>
     <div>
-        <a href=""><div><img src="{{asset('img/buffet.jpg')}}" alt=""><span>Serviço 2</span></img></div></a>
+        <a href=""><div><img src="{{asset('img/buffet.jpg')}}" alt=""><span>Serviço 2</span></div></a>
     </div>
     <div>
-        <a href=""><div><img src="{{asset('img/consultoria.jpg')}}" alt=""><span>Serviço 3</span></img></div></a>
+        <a href=""><div><img src="{{asset('img/consultoria.jpg')}}" alt=""><span>Serviço 3</span></div></a>
     </div>
 
 </section>
