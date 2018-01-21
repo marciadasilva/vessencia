0441e9bf37444466289e97527aa9f99da2aeb1a1
igorhaugg <igor-haugg@hotmail.com>
2018-01-21 09:44:57 -0200

    new layout

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,9 +4,9 @@
 
 @include('layouts.header')
 
-<main id="admin-page">
+<section id="admin-page">
   <div class="panel">
     <h1>Olá {{Auth::user()->name}} </h1>
     <h2>Bem vindo a área administrativa!</h2>
   </div>
-</main>
+</section>
