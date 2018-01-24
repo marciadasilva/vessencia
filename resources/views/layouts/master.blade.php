38c7c8191a5de7938d42f0c14ed23e35478f670d
igorhaugg <igor-haugg@hotmail.com>
2018-01-23 22:07:13 -0200

    chance config

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,8 +14,8 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
   <!-- <link rel="stylesheet" href="{{ asset('css/lightbox.min.css')}}"> -->
-  <link rel="stylesheet" href="{{ asset('css/app.css')}}" type="text/css">
-  <link rel="stylesheet" href="{{ asset('css/script.css')}}" type="text/css">
+  <link rel="stylesheet" href="/css/app.css" type="text/css">
+  <!-- <link rel="stylesheet" href="/css/script.css" type="text/css"> -->
   <title>@yield('title')</title>
 </head>
   <body>
@@ -24,12 +24,12 @@
 
     @include('layouts.footer')
 
-    <script src="{{ asset('js/app.js')}}"></script>
-    <script src="{{ asset('js/script.js')}}"></script>
+    <script src="/js/app.js"></script>
+    <script src="/js/script.js"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-    <script src="{{ asset('js/lightbox-plus-jquery.min.js')}}"></script>
+    <script src="/js/lightbox-plus-jquery.min.js"></script>
     @include('sweet::alert')
   </body>
 </html>
