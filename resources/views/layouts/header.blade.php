c28225a7576e9eaa7d07f8227816b90d72b36788
Marcia da Silva <marcia.dasilva@unijui.edu.br>
2018-02-07 10:12:39 -0200

    admin frontend

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -21,6 +21,7 @@
     <li><a class="expand_middle" href="/admin/menu">Cardápios</a></li>
     <li><a class="expand_middle" href="/admin/service">Serviços</a></li>
     <li><a class="expand_middle" href="/admin/company">Empresa</a></li>
+    <li><a class="expand_middle" href="/admin/news">Notícias</a></li>
     <li><a class="expand_middle" href="/admin/help">Ajuda</a></li>
     <li><a class="expand_middle" href="/logout">Sair</a></li>
   </nav>
@@ -34,7 +35,7 @@
     <li><a class="expand_middle" href="/menus">Cardápios</a></li>
     <li><a class="expand_middle" href="/services">Serviços</a></li>
     <li><a class="expand_middle" href="/gallery-photos">Galerias</a></li>
-    <li><a class="expand_middle" href="#news">Notícias</a></li>    
+    <li><a class="expand_middle" href="#news">Notícias</a></li>
     <li><a class="expand_middle" href="/contact">Contato</a></li>
     <li><a class="expand_middle" href="">EN</a></li>
   </nav>
