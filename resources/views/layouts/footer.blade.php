29e8b8ed6b889913f7fd0e154974aed075115015
Marcia <Marcia>
2018-02-13 21:58:21 -0200

    teste

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer>
   <p>5ª Essência Gastronomia e Eventos. </p>
-  <p>&copy Copyright: <a href="#"><strong> Márcia da Silva  |  Igor G. Haugg</strong></a></p>
+  <p>&copy Copyright: <a href="#"><strong> Márcia da Silva | Igor G. Haugg</strong></a></p>
   <ul>
     <li><a href="https://twitter.com/mds_marcia" target="_blank"><i class="fa fa-twitter-square fa-2x"></i></a></li>
     <li><a href="https://www.facebook.com/marciah.silvah" target="_blank"><i class="fa fa-facebook-square fa-2x"></i></a></li>
